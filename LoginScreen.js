84475a6a929921e7a8154152b5a547e7e4e28bc8
madjtech <teletype777@gmail.com>
2018-07-09 12:40:37 +0900

    first commit

---
+++ LoginScreen.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import {
+    View,
+    Text,
+    StyleSheet,
+} from 'react-native';
+export default class LoginInput extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                
+            </View>
+        );
+    }
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+});
